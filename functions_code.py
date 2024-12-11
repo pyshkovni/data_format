ce8d134f82a00a6d143609d7f4a3f4e2e4bf852a
skygazing1 <8120007@gmail.com>
2024-12-12 00:05:05 +0300

    перенос функции main в файл main

--- functions_code.py
+++ functions_code.py
@@ -21,14 +21,3 @@ def save_to_csv(data, output_filepath):
         writer.writerows(data)
 
 
-def main():
-
-    input_filepath = Path("/Users/h1pyy/PycharmProjects/data_case3/Rome24.09.06.json")
-    output_filepath = Path("Users/h1pyy/PycharmProjects/data_case3/output/sunrise_sunset.csv")
-
-    output_filepath.parent.mkdir(parents=True, exist_ok=True)
-
-    json_data = load_json(input_filepath)
-    extracted_data = extract_data(json_data)
-    save_to_csv(extracted_data, output_filepath)
-    print(f"Данные успешно сохранены в {output_filepath}")
