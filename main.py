81501398193717e3c443ac6bf9920fe12149f42a
Andrey1123413 <dimasous620@gmail.com>
2024-12-11 13:15:10 +0300

    2 ФУНКЦИЯ

--- main.py
+++ main.py
@@ -11,3 +11,21 @@ def read_json(file_path):
         data = json.load(json_file)
     return data
 
+
+def write_csv(data, output_path):
+    """
+    Записывает данные в CSV-файл в формате: link, company.
+    :param data: Данные для записи
+    :param output_path: Путь до выходного CSV-файла
+    """
+    with open(output_path, 'w', encoding='utf-8', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')  # Явно указываем разделитель — запятая
+        # Записываем заголовки таблицы
+        writer.writerow(['link', 'company'])
+        
+        # Записываем данные из JSON в CSV
+        for event in data:
+            link = event.get('link')
+            company = event.get('company')
+            if link and company:
+                writer.writerow([link, company])
