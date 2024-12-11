f1b4ed29dc7834f0dce3e018dbced629d1cc742e
Krupkin Maksim <krupkinmacsim@yandex.ru>
2024-12-11 11:39:46 +0300

    Добавил код который считает количество строк и записывает их в файл

--- 13var.py
+++ 13var.py
@@ -1,18 +1,26 @@
 import csv
 import os
 
-folder_name = '/Users/maksimkrupkin/case/data_format/data'
-file_name = 'weekly_IBM.csv'
-
-import csv
-import os
-
 
 def count_csv_rows(folder_name, file_name):
+    folder_name = '/Users/maksimkrupkin/case/data_format/data'
+    file_name = 'weekly_IBM.csv'
 
     file_path = os.path.join(folder_name, file_name)
 
     with open(file_path, mode='r', newline='', encoding='utf-8') as file:
             csv_reader = csv.reader(file)
             
+            # Подсчёт количества строк в файле
+            row_count = sum(1 for row in csv_reader)
+            
+            with open('number.txt', 'w') as file:
+                # Записываем число в файл
+                file.write(str(row_count))
+                print("Число сохранено в файл 'number.txt'")
+
+folder_name = '/Users/maksimkrupkin/case/data_format/data'
+file_name = 'weekly_IBM.csv'
+row_count = count_csv_rows(folder_name, file_name)
+
 
