b72aa7f46bf625c8bfff834a2d1e2a8113c7ddcf
Krupkin Maksim <krupkinmacsim@yandex.ru>
2024-12-11 11:39:09 +0300

    Создаем функцию которая открывает файл и считает количество строк

--- 13var.py
+++ 13var.py
@@ -4,3 +4,15 @@ import os
 folder_name = '/Users/maksimkrupkin/case/data_format/data'
 file_name = 'weekly_IBM.csv'
 
+import csv
+import os
+
+
+def count_csv_rows(folder_name, file_name):
+
+    file_path = os.path.join(folder_name, file_name)
+
+    with open(file_path, mode='r', newline='', encoding='utf-8') as file:
+            csv_reader = csv.reader(file)
+            
+
