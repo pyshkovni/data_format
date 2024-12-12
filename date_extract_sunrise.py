5d5dbc2bf5f69005fa25969c90304f96bdfe6281
dashunikk <dariashhh5@gmail.com>
2024-12-12 17:57:47 +0300

    создание папки + рабочая функция

--- date_extract_sunrise.py
+++ date_extract_sunrise.py
@@ -23,3 +23,20 @@ def save_to_csv(data, output_filepath):
     for row in data:
         writer.writerow(row)
     file.close()
+
+def main():
+    #пути до JSON-файла и CSV-файла
+    input_filepath = Path("data/Rome24.09.06.json")
+    output_filepath = Path("output/sunrise_sunset.csv")
+    
+    #создание папки output, если её не существует
+    output_filepath.parent.mkdir(parents=True, exist_ok=True)
+
+    #загрузка и обработка данных
+    json_data = load_json(input_filepath)
+    extracted_data = extract_data(json_data)
+    save_to_csv(extracted_data, output_filepath)
+    print("Данные успешно сохранены в " + str(output_filepath))
+
+if __name__ == "__main__":
+    main()
