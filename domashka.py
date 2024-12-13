f7e557ade42a961cad9bfba835d6ba76deb9bb8f
Ernest <ernest.evt@gmail.com>
2024-12-13 18:22:04 +0300

    Исправил, сделал индекс

--- domashka.py
+++ domashka.py
@@ -1,4 +1,7 @@
-
 import pandas as pd
 csv_file = '/Users/ernestevtuhov/PycharmProjects/data_format/data/fx_weekly_EUR_USD.csv'
 data = pd.read_csv(csv_file)
+
+data['timestamp'] = pd.to_datetime(data['timestamp'])
+
+data.set_index('timestamp', inplace=True)
