d6eb23051f16799e54d111a5dcc9f6d07ca8fb5b
Ernest <ernest.evt@gmail.com>
2024-12-13 18:23:07 +0300

    Сгруппировал по годам и нашел макс

--- domashka.py
+++ domashka.py
@@ -3,5 +3,7 @@ csv_file = '/Users/ernestevtuhov/PycharmProjects/data_format/data/fx_weekly_EUR_
 data = pd.read_csv(csv_file)
 
 data['timestamp'] = pd.to_datetime(data['timestamp'])
-
 data.set_index('timestamp', inplace=True)
+
+max_open = data.resample('YE')['open'].max()
+max_close = data.resample('YE')['close'].max()
